a03c0e6a9e7c67228bff590dbd63adf963650ccf
spencehood <spho6674@colorado.edu>
2015-03-08 17:54:34 -0600

    final submission for api work

--- contents/activities/index.js
+++ contents/activities/index.js
@@ -6,12 +6,13 @@ var activities = {
         var id = id || '101193'
 
         //$.get("https://vimeo.com/api/v2/activity/"+ id +"/user_did.json", function(data) {
-        $.get("http://api.nytimes.com/svc/books/v3/lists", function(data) {
+            $.get("http://api.nytimes.com/svc/books/v3/lists", function(data) {
+            //$.get("http://github.com/users/spencehood/nyt.git"), function(data) {
 
                 console.log('now ' + data)
                 
                 //$.get("/vimeo/activities/list.jade", function(template) {
-                $.get("/indiv-final/activities/list.jade", function(template) {
+                $.get("/nytimes/activities/list.jade", function(template) {
                 
                     var html = jade.render(template, {
                         data: data
@@ -26,7 +27,7 @@ var activities = {
     load: function() {
 
         //$.get("/vimeo/activities/ui.jade", function(template) {
-        $.get("/indiv-final/activities/ui.jade", function(template) {
+        $.get("/nytimes/activities/ui.jade", function(template) {
 
             var html = jade.render(template)
             $("#ui").html(html)
@@ -34,6 +35,8 @@ var activities = {
 
         // default search results
         activities.searchByUserId('101193')
+
+        nyt.best_sellers.get({'query':'girl'}, console.log);
 
     }
 
--- contents/videos/index.js
+++ contents/videos/index.js
@@ -5,12 +5,13 @@ var videos = {
         // search videos by userId (default user, 101193)
         var id = id || '101193'
                
-        $.get("https://vimeo.com/api/v2/" + id + "/videos.json", function(data) {
+        //$.get("https://vimeo.com/api/v2/" + id + "/videos.json", function(data) {
+        $.get("http://api.nytimes.com/svc/movies/v2/reviews", function(data) {
             
             console.log('now ' + data)
             
             if (data){
-            $.get("/vimeo/videos/list.jade", function(template) {
+            $.get("/nytimes/videos/list.jade", function(template) {
                 var html = jade.render(template, {
                     data: data
                 });
@@ -24,7 +25,9 @@ var videos = {
   
     load: function() {
 
-        $.get("/vimeo/videos/ui.jade", function(template) {
+        //$.get("/vimeo/videos/ui.jade", function(template) {
+        $.get("/nytimes/videos/ui.jade", function(template) {
+
             var html = jade.render(template)
             $("#ui").html(html)
         })
